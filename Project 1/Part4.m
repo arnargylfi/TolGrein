4da9670778fc7eace2b84a47511d01ab5ba00308
Arnar Gylfi <arnargylfi01@gmail.com>
2023-11-28 14:08:20 +0000

    Part 7

--- Project 1/Part4.m
+++ Project 1/Part4.m
@@ -3,15 +3,15 @@ T0 = 273.16; %Fasti
 %Gögn: 
 T = [20,40,60,80,100]';
 mu = 10^(-3)*[1.003, 0.657, 0.467, 0.355, 0.283]';
-%Linearization 
-ln_mu = log(mu);    
+%Linearization
+ln_mu = log(mu);
 
 %Stilli upp stuðla fylki sem á að margfaldast við [a,b,c] og jafngilda ln(mu) 
 
 A = [ones(length(T),1),1./(T+T0),1./(T+T0).^2];
 b = ln_mu;
 
-%RMSQ aðferð: 
+%RMSQ aðferð:
 
 x = (A'*A)\A'*b;
 scatter(T',mu','filled')
