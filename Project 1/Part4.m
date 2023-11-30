becde160a60816705b10706f5b7cdc3bfe79ed79
G0rocks <huldarh@gmail.com>
2023-11-30 15:07:19 +0000

    Merkja ása á grafi

--- Project 1/Part4.m
+++ Project 1/Part4.m
@@ -21,9 +21,8 @@ T_2 = T_2';
 A_2 = [ones(length(T_2),1),1./(T_2+T0),1./(T_2+T0).^2];
 mu_2 = exp(A_2*x);
 plot(T_2,mu_2);
+xlabel("Hitastig [°C]")
+ylabel("Seigja [Ns/m^2]")
 
 RMSE = sqrt(sum((exp(A*x)-mu).^2*1)/length(A));
-
-
-
 
