d58a37bfdf13d6af480c82930fbbffa6e7a31068
G0rocks <huldarh@gmail.com>
2023-12-01 14:10:33 +0000

    Bæta við grafi og samanburði við dæmi 10 í aukadæmi

--- Project 1/Independent work/attemptRUN.m
+++ Project 1/Independent work/attemptRUN.m
@@ -8,3 +8,11 @@ mu = 1.002*10^(-3);
 q_result = newtonmult(q0,10^(-7),d,delta_p0)
 
 Reynolds = (4*rho*q_result)./(mu*pi*D);
+
+% Gerum stöplarit til að bera saman niðurstöðurnar úr dæmi 10 við þessar
+barplotdata = compare_in_bar_plot_extra(q10, q_result);
+bar(barplotdata)
+title("Flæðissamanburður")
+xlabel("Númer pípu")
+ylabel("Flæði [m^3/s]")
+legend({"Dæmi 10", "Flæði með gati"}, 'Location', 'north')
