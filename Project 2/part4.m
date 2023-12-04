5b7900d7d43086c2c70bacf1b0303e8be479fba0
Steinarr134 <steinarrrrrrrrrr@gmail.com>
2023-12-04 21:55:46 +0000

    moved part 4 into function and created script that calls it to plot different starting values

--- Project 2/part4.m
+++ Project 2/part4.m
@@ -1,22 +1,10 @@
-s0 = 0;
-theta0 = pi/12;
-T = 10;
-n = 300;
-w = RKsolver(s0,theta0,T,n)
-plot(w(:,1),w(:,3))
-xlabel('x [m]')
-ylabel('y [m]')
-title("Pendúll dæmi 4");
-% Bæta við upphafspunkti og endapunkti ferils
-hold on
-plot(w(1,1),w(1,3),'r*');   % Upphafspunktur
-text(w(1,1),w(1,3)+0.03,"t = 0 sek", 'fontsize', 14);
 
-plot(w(n+1,1),w(n+1,3),'r*');   % Upphafspunktur
-text(w(n+1,1),w(n+1,3)+0.03,"t = " + T + " sek", 'fontsize', 14);
+figure(1)
+part4_plot(0, pi/12, 10)
+title("s_0 = 0, \theta_0=\pi/12, T=10", 'Interpreter','tex');
 
-% Hlutfallslega jafn stórir ásar
-daspect([1 1 1])
-% Stilla textastærð
-set(gca,'FontSize',15)
-hold off
+figure(2)
+part4_plot(0.4, pi/12, 10)
+% title(...)
+
+figure(3)
---
+++ Project 2/part4_plot.m
@@ -0,0 +1,26 @@
+function part4_plot(s0, theta0, T)
+% s0 = 0;
+% theta0 = pi/12;
+% T = 10;
+n = 300;
+w = RKsolver(s0,theta0,T,n)
+plot(w(:,1),w(:,3))
+xlabel('x [m]')
+ylabel('y [m]')
+% title("s_0 = 0, \theta_0=\pi/12, T=10", 'Interpreter','tex');
+% Bæta við upphafspunkti og endapunkti ferils
+hold on
+plot(w(1,1),w(1,3), 'b<', 'MarkerSize', 10, 'MarkerFaceColor', 'g');   % Upphafspunktur
+% text(w(1,1),w(1,3)+0.03,"t = 0", 'fontsize', 14);
+
+plot(w(n+1,1),w(n+1,3),'bsquare', 'MarkerSize', 10, 'MarkerFaceColor', 'r');   % Upphafspunktur
+% text(w(n+1,1),w(n+1,3)+0.03,"t = " + T + " sek", 'fontsize', 14);
+
+% Hlutfallslega jafn stórir ásar
+daspect([1 1 1])
+% Stilla textastærð
+set(gca,'FontSize',15)
+w_y_diff = max(w(:, 3)) - min(w(:, 3))
+ylim([min(w(:,3)) - w_y_diff*0.1, max(w(:,3) + w_y_diff*0.1)])
+hold off
+end
