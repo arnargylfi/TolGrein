3fb044125498b6f6d9dc8a0ea1b11de8b5f86d72
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-04 16:39:24 +0000

    epic animation í part 3

--- Project 2/part3.m
+++ Project 2/part3.m
@@ -6,10 +6,13 @@ x = w(:,1); y = w(:,3);
 
 
 ball = animatedline('color','b','Marker','.','markersize',20);
+path = animatedline('Color','m','LineStyle','-'); % 'm' for magenta, you can choose any color
+hLine = line([0, x(1)], [0, y(1)], 'Color', 'r'); %initialize línu frá (0,0) til fyrstu stöðu massans (gpt kóði smá skrýtið) 
 for k = 1:n+1
+    addpoints(path,x(k),y(k));
     clearpoints(ball)
     addpoints(ball,x(k),y(k));
+    set(hLine, 'XData', [0, x(k)], 'YData', [0, y(k)]); % updatea endapunkt línunnar alltaf 
     pause(0.1) %optional, changes speed
-    line([0, x(k)], [0, y(k)], 'Color', 'r'); % Draw line in red for contrast
     drawnow %set to comment if only final picture is needed
 end
