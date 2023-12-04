884c5f163920aa18e093a97d06e568586bb21c99
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-04 14:26:43 +0000

    Part 2 rétt

--- Project 2/Eulersolver.m
+++ Project 2/Eulersolver.m
@@ -1,11 +1,34 @@
 function w=Eulersolver(s0,theta0,T,n) %might need more inputs later
 %calculate initial position x0, y0 and step size h
-
-L_eq = L0 + (m*g)/k;
+m = 0.2;
+k = 2.5; %gormfasti
+L0 = 1; %óteigð gormalengd
+g = 9.81;
+Leq = L0 + (m*g)/k;
 x0 = (Leq+s0)*sin(theta0);
-y0 = (Leq+s0)*cos(theta0);
-w(1,:)=[x0,0,y0,0] %initial conditions
+y0 = -(Leq+s0)*cos(theta0);
+h = T/n;
+w(1,:)=[x0,0,y0,0]; %initial conditions
 for i=1:n
      w(i+1,:)=w(i,:)+eulerstep(w(i,:),h);
 end
+end
+
+
+function z=eulerstep(w,h) %will eventually be replaced by RKstep(w,h)
+z=h*system(w);
+end
+function z=system(w)
+m = 0.2;
+k = 2.5; %gormfasti
+L0 = 1; %óteigð gormalengd
+g = 9.81;
+L = sqrt(w(1)^2+w(3)^2);
+% the differential system
+z(1)= w(2);
+z(2)= -(k/m)*(L-L0)*w(1)/L;
+z(3)= w(4);
+z(4)= -(k/m)*(L-L0)*w(3)/L-g;
+end  
+
 
--- Project 2/part2run.m
+++ Project 2/part2run.m
@@ -1,10 +1,6 @@
-m = 0.2;
-k = 2.5; %gormfasti
-L0 = 1; %óteigð gormalengd
-g = 9.81;
-xy = [4;3]; %x(t), y(t)
-L = norm(xy); %lengd gorms (sqrt(x^2+y^2))
-deltaL = L-L0; %teiging á gorm
-n = (1/L)*xy;  %Einhver fasti sem er reiknaður til að reikna kraftinn að neðan
-F = -k*deltaL*n; %kraftur 
-
+s0 = 0;
+theta0 = pi/12;
+w = Eulersolver(s0,theta0,10,300)
+plot(w(:,1),w(:,3))
+xlabel('x [m]')
+ylabel('y [m]')
