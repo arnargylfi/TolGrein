c9cb7b06f27e524d5234d38c59a76a572e4919a7
G0rocks <huldarh@gmail.com>
2023-12-04 16:12:06 +0000

    Uppfæra graf í dæmi 2 verkefni 2

--- Project 2/part2run.m
+++ Project 2/part2run.m
@@ -1,6 +1,22 @@
 s0 = 0;
 theta0 = pi/12;
-w = Eulersolver(s0,theta0,10,300)
+T = 10;
+n = 300;
+w = Eulersolver(s0,theta0,T,n)
 plot(w(:,1),w(:,3))
 xlabel('x [m]')
 ylabel('y [m]')
+title("Pendúll dæmi 2");
+% Bæta við upphafspunkti og endapunkti ferils
+hold on
+plot(w(1,1),w(1,3),'r*');   % Upphafspunktur
+text(w(1,1),w(1,3)+0.03,"t = 0 sek", 'fontsize', 14);
+
+plot(w(n+1,1),w(n+1,3),'r*');   % Upphafspunktur
+text(w(n+1,1),w(n+1,3)+0.03,"t = " + T + " sek", 'fontsize', 14);
+
+% Hlutfallslega jafn stórir ásar
+daspect([1 1 1])
+% Stilla textastærð
+set(gca,'FontSize',15)
+hold off
