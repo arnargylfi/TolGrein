52630e2f93b896c6641c83447edb1d02633464ce
G0rocks <huldarh@gmail.com>
2023-12-05 11:22:06 +0000

    Fikta í gröfum dæmi 6

--- Project 2/part6.m
+++ Project 2/part6.m
@@ -32,18 +32,21 @@ y0 = -(Leq+s0)*cos(theta0);
 Einitial = Etotal(1)
 
 Energy_error = abs(Einitial-Etotal);
-t = 0:40/300:40;
+t = 0:40/n:40;
 plot(t,[Etotal,U,K,W,Energy_error])
+title(['Orkugreining pendúls, n = ', num2str(n)]);
 yline(Einitial,'Label','Upphafsorka')
 xlabel('Tími [sek]');
 ylabel('Orka [Júl]');
-legend('Heildarorka','Stöðuorka','Hreyfiorka','Fjaðurorka','Orkutap', 'Location','best');
+legend('Heildarorka','Stöðuorka','Hreyfiorka','Fjaðurorka','Orkutap/Skekkja', 'Location','best');
 
 % Annað graf með subplots
 figure(2);
-subplot(2,2,1);  % Orkutap
+% Super title
+sgtitle(['n = ', num2str(n)]);
+subplot(2,2,1);  % Orkutap/Skekkja
 plot(t,Energy_error);
-title('Orkutap');
+title('Orkutap/Skekkja');
 xlabel('Tími [sek]');
 ylabel('Orka [Júl]')
 
@@ -66,3 +69,66 @@ xlabel('Tími [sek]');
 ylabel('Orka [Júl]')
 
 
+% Aftur með mun stærra n
+%----------------------------------------------------------------------------
+n = n*2;        % Skrefastærð   
+% leysum með fyrri aðferðum
+w = RKsolver(s0,theta0,T,n);
+
+% til að einfalda jöfnurnar, drögum út:
+x = w(:,1); dx = w(:,2); y = w(:,3); dy = w(:,4);
+
+% Stöðuorka
+U = m*g*y;
+
+% Hreyfiorka 
+K = 0.5*m*(dx.^2+dy.^2); % verkefnalýsing er vitlaus
+
+% Fjaðurorka
+W = 0.5*k*(L0-sqrt(x.^2+y.^2)).^2;
+
+Etotal = U+K+W;
+
+Leq = L0 + (m*g)/k;
+x0 = (Leq+s0)*sin(theta0);
+y0 = -(Leq+s0)*cos(theta0);
+% Einitial = -m*g*y0+k*(L0-sqrt(x0^2+y0^2))^2/2;
+Einitial = Etotal(1)
+
+Energy_error = abs(Einitial-Etotal);
+t = 0:40/n:40;
+figure(3);
+plot(t,[Etotal,U,K,W,Energy_error])
+yline(Einitial,'Label','Upphafsorka')
+title(['Orkugreining pendúls, n = ', num2str(n)]);
+xlabel('Tími [sek]');
+ylabel('Orka [Júl]');
+legend('Heildarorka','Stöðuorka','Hreyfiorka','Fjaðurorka','Orkutap/Skekkja', 'Location','best');
+
+% Annað graf með subplots
+figure(4);
+% Super title
+sgtitle(['n = ', num2str(n)]);
+subplot(2,2,1);  % Orkutap/Skekkja
+plot(t,Energy_error);
+title('Orkutap/Skekkja');
+xlabel('Tími [sek]');
+ylabel('Orka [Júl]')
+
+subplot(2,2,2);  % Stöðuorka
+plot(t,U);
+title('Stöðuorka');
+xlabel('Tími [sek]');
+ylabel('Orka [Júl]')
+
+subplot(2,2,3);  % Hreyfiorka
+plot(t,K);
+title('Hreyfiorka')
+xlabel('Tími [sek]');
+ylabel('Orka [Júl]')
+
+subplot(2,2,4);  % Fjaðurorka
+plot(t,W);
+title('Fjaðurorka');
+xlabel('Tími [sek]');
+ylabel('Orka [Júl]')
