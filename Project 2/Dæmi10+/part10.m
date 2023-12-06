316e34c306f6c2a532bfa1ea4aa8ad279438490c
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-06 14:43:19 +0000

    dæmi 10 mjög rétt

--- Project 2/Dæmi10+/part10.m
+++ Project 2/Dæmi10+/part10.m
@@ -1,21 +1,28 @@
 T = 60;
 n = 18000;
-y_diff = 0:0.1:10;
+y_diff = 0:0.05:10;
 s0 = 0.5;
 theta0 = pi/3;
 epsilon = 10^(-3);
+maxskekkjur = zeros(length(y_diff),1);
 for i = 1:length(y_diff)
     clf;
     w = RKsolver(s0,theta0,T,n,y_diff(i));
     w2 = RKsolver(s0+epsilon,theta0+epsilon,T,n,y_diff(i));
     subplot(1,2,1)
     plot(w(:,1),w(:,3))
-    title('asdæij')
+    title("Án \epsilon", "interpreter" , "tex");
     xlabel('x')
     ylabel('y')
     subplot(1,2,2)
-    plot (w2(:,1),w2(:,3))
-    title('asijd')
-    sgtitle('Gröf')
-    pause(0.1);
+    plot (w2(:,1),w2(:,3),'m')
+    title("Með \epsilon", "interpreter" , "tex");
+    skekkjur = sqrt((w2(:,1)-w(:,1)).^2+(w2(:,3)-w(:,3)).^2);
+    maxskekkjur(i) = max(skekkjur);
+    sgtitle("y'(0) = " + y_diff(i)+" Hámarks frávik milli ferla = "+ max(skekkjur))
+    pause(0);
 end % for
+plot(y_diff,maxskekkjur)
+xlabel("y'(0)")
+ylabel('Hámarks frávik milli tveggja ferla')
+
