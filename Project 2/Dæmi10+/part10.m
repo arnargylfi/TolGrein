d848be72546c19fce53d04a0e91c829c2cc9672b
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-06 13:41:37 +0000

    viðbætur D10

--- Project 2/Dæmi10+/part10.m
+++ Project 2/Dæmi10+/part10.m
@@ -1,11 +1,17 @@
 T = 60;
 n = 18000;
-y_diff = 0.5;
+y_diff = 0.1;
 s0 = 0.5;
 theta0 = pi/3;
 epsilon = 10^(-3);
 w = RKsolver(s0,theta0,T,n,y_diff);
 w2 = RKsolver(s0+epsilon,theta0+epsilon,T,n,y_diff);
+subplot(1,2,1)
 plot(w(:,1),w(:,3))
-hold on
+title('asdæij')
+xlabel('x')
+ylabel('y')
+subplot(1,2,2)
 plot (w2(:,1),w2(:,3))
+title('asijd')
+sgtitle('Gröf')
