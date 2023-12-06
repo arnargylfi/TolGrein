e550f85a2306f37fb6c02f45430a22e9865ee7c8
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-06 13:54:38 +0000

    dæmi 10 virkandi núna

--- Project 2/Dæmi10+/part10.m
+++ Project 2/Dæmi10+/part10.m
@@ -1,17 +1,21 @@
 T = 60;
 n = 18000;
-y_diff = 0.1;
+y_diff = 0:0.1:10;
 s0 = 0.5;
 theta0 = pi/3;
 epsilon = 10^(-3);
-w = RKsolver(s0,theta0,T,n,y_diff);
-w2 = RKsolver(s0+epsilon,theta0+epsilon,T,n,y_diff);
-subplot(1,2,1)
-plot(w(:,1),w(:,3))
-title('asdæij')
-xlabel('x')
-ylabel('y')
-subplot(1,2,2)
-plot (w2(:,1),w2(:,3))
-title('asijd')
-sgtitle('Gröf')
+for i = 1:length(y_diff)
+    clf;
+    w = RKsolver(s0,theta0,T,n,y_diff(i));
+    w2 = RKsolver(s0+epsilon,theta0+epsilon,T,n,y_diff(i));
+    subplot(1,2,1)
+    plot(w(:,1),w(:,3))
+    title('asdæij')
+    xlabel('x')
+    ylabel('y')
+    subplot(1,2,2)
+    plot (w2(:,1),w2(:,3))
+    title('asijd')
+    sgtitle('Gröf')
+    pause(0.1);
+end % for
