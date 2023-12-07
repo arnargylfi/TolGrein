90f0aa3dfa09334cb8fb951ffb167222f4d75b17
G0rocks <huldarh@gmail.com>
2023-12-07 12:40:39 +0000

    part4_plot titlar viðbót

--- Project 2/part4.m
+++ Project 2/part4.m
@@ -7,20 +7,17 @@ T = 20;
 figure(1)
 s0 = 0;
 theta0 = pi/12; % Muna að skrifa líka í titil grafs
-part4_plot(s0, theta0, T)
-title("s_0 = " + s0 + "      \theta_0 = \pi/12      T = " + T, 'Interpreter','tex');
+part4_plot(s0, theta0, T, "s_0 = " + s0 + "      \theta_0 = \pi/12      T = " + T);
 
 
 % Graf 2
 figure(2)
 s0 = -0.2;
 theta0 = -pi/10; % Muna að skrifa líka í titil grafs
-part4_plot(s0, theta0, T)
-title("s_0 = " + s0 + "      \theta_0 = -\pi/10      T = " + T, 'Interpreter','tex');
+part4_plot(s0, theta0, T,"s_0 = " + s0 + "      \theta_0 = -\pi/10      T = " + T);
 
 % Graf 3
 figure(3)
 s0 = 0.2;
 theta0 = pi/9; % Muna að skrifa líka í titil grafs
-part4_plot(s0, theta0, T)
-title("s_0 = " + s0 + "      \theta_0 = \pi/9      T = " + T, 'Interpreter','tex');
+part4_plot(s0, theta0, T, "s_0 = " + s0 + "      \theta_0 = \pi/9      T = " + T);
--- Project 2/part4_plot.m
+++ Project 2/part4_plot.m
@@ -1,4 +1,4 @@
-function part4_plot(s0, theta0, T)
+function part4_plot(s0, theta0, T, titill)
 % s0 = 0;
 % theta0 = pi/12;
 % T = 10;
@@ -16,7 +16,11 @@ plot(w(1,1),w(1,3), 'b<', 'MarkerSize', 10, 'MarkerFaceColor', 'g');   % Upphafs
 plot(w(n+1,1),w(n+1,3),'bsquare', 'MarkerSize', 10, 'MarkerFaceColor', 'r');   % endapunktur
 % text(w(n+1,1),w(n+1,3)+0.03,"t = " + T + " sek", 'fontsize', 14);
 
+% Legend
 legend('', 't = 0sek', 't = T', 'Location', 'Best')
+% Title
+title(titill, 'Interpreter','tex');
+
 
 % Hlutfallslega jafn stórir ásar
 daspect([1 1 1])
