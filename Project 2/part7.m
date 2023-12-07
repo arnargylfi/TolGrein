c48c5de97fa478449cb89844207872334586e60f
G0rocks <huldarh@gmail.com>
2023-12-07 15:55:18 +0000

    Dæmi 7 og 8 vista gröf

--- Project 2/part7.m
+++ Project 2/part7.m
@@ -1,4 +1,6 @@
 tic
+close all % Loka öllum gröfum
+savetime = true;
 m = 0.2;    % Massi             [kg]
 k = 2.5;    % gormfasti         [N/m]
 L0 = 1;     % óteygð gormalengd [m]
@@ -12,42 +14,61 @@ n = 10*T; %Leiðbeiningar samkvæmt verkefnalýsingu, byrjar í 10 T og tvöfald
 
 % leysum með fyrri aðferðum
 % for lykkja sem leysir fyrir allar skrefastærðir og vistar niðurstöðuna
-Energy_error = zeros(8,1);
-for i = 1:9
-    w = RKsolver(s0,theta0,T,n);
+if not(savetime)
+    Energy_error = zeros(9,1);
+    h = zeros(9,1);
+    for i = 1:9
+        h(i) = T/n;
+        w = RKsolver(s0,theta0,T,n);
+    
+        % til að einfalda jöfnurnar, drögum út:
+        x = w(:,1); dx = w(:,2); y = w(:,3); dy = w(:,4);
+    
+        % Stöðuorka
+        U = m*g*y;
+    
+        % Hreyfiorka 
+        K = 0.5*m*(dx.^2+dy.^2); % verkefnalýsing er vitlaus
+    
+        % Fjaðurorka
+        W = 0.5*k*(L0-sqrt(x.^2+y.^2)).^2;
+    
+        Etotal = U+K+W;
+    
+        Leq = L0 + (m*g)/k;
+        x0 = (Leq+s0)*sin(theta0);
+        y0 = -(Leq+s0)*cos(theta0);
+        Einitial = m*g*y0+k*(L0-sqrt(x0^2+y0^2))^2/2;
+    %     Einitial = Etotal(1);
+        Energy_error(i) = abs(Etotal(n)-Einitial);
+        nlist(i) = n;
+        n = n*2;
+    end % For
+end % if
 
-    % til að einfalda jöfnurnar, drögum út:
-    x = w(:,1); dx = w(:,2); y = w(:,3); dy = w(:,4);
-
-    % Stöðuorka
-    U = m*g*y;
-
-    % Hreyfiorka 
-    K = 0.5*m*(dx.^2+dy.^2); % verkefnalýsing er vitlaus
-
-    % Fjaðurorka
-    W = 0.5*k*(L0-sqrt(x.^2+y.^2)).^2;
-
-    Etotal = U+K+W;
-
-    Leq = L0 + (m*g)/k;
-    x0 = (Leq+s0)*sin(theta0);
-    y0 = -(Leq+s0)*cos(theta0);
-    Einitial = m*g*y0+k*(L0-sqrt(x0^2+y0^2))^2/2;
-%     Einitial = Etotal(1);
-    Energy_error(i) = abs(Einitial-Etotal(n));
-    nlist(i) = n;
-    n = n*2;
-end % For
-
-% plot(1:9, Energy_error);
-% xticklabels(arrayfun(@num2str, nlist, 'UniformOutput', false));
-% xlabel('Skrefafjöldi n');
-% ylabel('Orka [Júl]');
-
-loglog(log(nlist), log(Energy_error));
+loglog(nlist, Energy_error);
 grid on
-xlabel('log(Skrefafjöldi n)');
-ylabel('log(Orkutap/skekkja) [log(Júl)]');
+xlabel('Skrefafjöldi n');
+ylabel('Orkutap/skekkja [Júl]');
+title('Orkutap sem fall af skrefafjölda. Log skala graf');
+% Vista mynd
+exportgraphics(gcf,'myndir/part7_1.pdf');
+
+% Finnum stig aðferðar Runge-Kutta
+logh = log(h);
+halli_og_fasti = Energy_error;
+halli_og_fasti = log(halli_og_fasti);
+halli_og_fasti = halli_og_fasti/logh;
+
+figure(2);
+plot(0:8,halli_og_fasti);
+title('Stig + log(C)/log(h)');
+xlabel('Ítrun á tvöföldun ');
+ylabel('log(Error)/log(h)');
+
+halli_og_fasti(8)-halli_og_fasti(1)
+
+% Vista mynd
+exportgraphics(gcf,'myndir/part7_2.pdf');
 
 toc
