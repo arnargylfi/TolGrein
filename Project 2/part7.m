d3dd799268332b040bcb44b5dc7864c11b874b7d
G0rocks <huldarh@gmail.com>
2023-12-07 17:17:45 +0000

    Dæmi 7 laga bug um nlist

--- Project 2/part7.m
+++ Project 2/part7.m
@@ -1,6 +1,8 @@
 tic
 close all % Loka öllum gröfum
-savetime = true;
+savetime = false;
+tvofaldanir = 9;    % Fjöldi skipta sem við tvöföldum n
+
 m = 0.2;    % Massi             [kg]
 k = 2.5;    % gormfasti         [N/m]
 L0 = 1;     % óteygð gormalengd [m]
@@ -12,12 +14,14 @@ theta0 = pi/12; % Horn          [rad]
 T = 40;         % Lokatími      [sek]
 n = 10*T; %Leiðbeiningar samkvæmt verkefnalýsingu, byrjar í 10 T og tvöfaldast í hvert sinn
 
+
 % leysum með fyrri aðferðum
 % for lykkja sem leysir fyrir allar skrefastærðir og vistar niðurstöðuna
 if not(savetime)
-    Energy_error = zeros(9,1);
-    h = zeros(9,1);
-    for i = 1:9
+    Energy_error = zeros(tvofaldanir + 1,1);
+    nlist = zeros(tvofaldanir + 1, 1);
+    h = zeros(tvofaldanir + 1,1);
+    for i = 1:(tvofaldanir+1)
         h(i) = T/n;
         w = RKsolver(s0,theta0,T,n);
     
@@ -43,6 +47,9 @@ if not(savetime)
         Energy_error(i) = abs(Etotal(n)-Einitial);
         nlist(i) = n;
         n = n*2;
+
+        % Progress report
+        fprintf('%.0f %%\n', 100*i/(tvofaldanir+1));
     end % For
 end % if
 
@@ -54,21 +61,48 @@ title('Orkutap sem fall af skrefafjölda. Log skala graf');
 % Vista mynd
 exportgraphics(gcf,'myndir/part7_1.pdf');
 
+figure(2);
+loglog(h, Energy_error);
+grid on
+xlabel('Skrefastærð h');
+ylabel('Orkutap/skekkja [Júl]');
+title('Orkutap sem fall af skrefastærð. Log skala graf');
+% Vista mynd
+exportgraphics(gcf,'myndir/part7_2.pdf');
+
 % Finnum stig aðferðar Runge-Kutta
-logh = log(h);
+logh = log10(h);
 halli_og_fasti = Energy_error;
-halli_og_fasti = log(halli_og_fasti);
+halli_og_fasti = log10(halli_og_fasti);
 halli_og_fasti = halli_og_fasti/logh;
 
-figure(2);
-plot(0:8,halli_og_fasti);
+figure(3);
+plot(0:tvofaldanir,halli_og_fasti);
 title('Stig + log(C)/log(h)');
 xlabel('Ítrun á tvöföldun ');
 ylabel('log(Error)/log(h)');
 
-halli_og_fasti(8)-halli_og_fasti(1)
+% Vista mynd
+exportgraphics(gcf,'myndir/part7_3.pdf');
+
+
+% Diffrum línu á mynd 1
+diffrid = zeros(tvofaldanir,1);
+stig = 0;
+for i = 1:tvofaldanir
+    diffrid(i) = (log(Energy_error(i+1))-log(Energy_error(i)))/(log(h(i+1))-log(h(i)));
+    stig = stig + diffrid(i);
+end % for
+
+stig/tvofaldanir
+
+figure(4);
+plot(0:tvofaldanir-1, diffrid);
+title('Diffrað log(Error)');
+xlabel('Tvöföldun');
+ylabel('Stig aðferðar');
 
 % Vista mynd
-exportgraphics(gcf,'myndir/part7_2.pdf');
+exportgraphics(gcf,'myndir/part7_4.pdf');
 
 toc
