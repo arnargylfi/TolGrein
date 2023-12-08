21e7f0d1ec7c9d529d38072929b54d4868ba98a1
G0rocks <huldarh@gmail.com>
2023-12-08 09:53:52 +0000

    Dæmi 7 fleiri og flottari gröf

--- Project 2/part7.m
+++ Project 2/part7.m
@@ -1,6 +1,6 @@
 tic
 close all % Loka öllum gröfum
-savetime = false;
+savetime = true;
 tvofaldanir = 8;    % Fjöldi skipta sem við tvöföldum n
 
 m = 0.2;    % Massi             [kg]
@@ -115,11 +115,33 @@ hold on;
 loglog(nlist, exp(polyval(p, log(nlist))), '-');
 xlabel('Skrefafjöldi n');
 ylabel('Orkutap/Skekkja Error');
-%legend('E vs n','Besta lína');
-legend('E vs n', ['Besta lína (halli: ', num2str(p(1)), ')']);
+%legend('Error(n)','Besta lína');
+legend('Error(n)', ['Besta lína (halli: ', num2str(p(1)), ')']);
 title('Orkutap sem fall af skrefafjölda. Log skala graf. ');
 grid on;
 hold off;
+% Vista mynd
+exportgraphics(gcf,'myndir/part7_5.pdf');
+% Prentum hallann
+disp(['Hallatala línunnar er: ', num2str(p(1))]);
+
+% Reiknum hallatöluna af Error(h) út frá bestu línu í log-log grafinu
+figure(6);
+p = polyfit(log(h), log(Energy_error), 1); % p(1) inniheldur hallann
+% Plottum upp
+loglog(h, Energy_error, 'o-');
+hold on;
+% Plottum bestu línu 
+loglog(h, exp(polyval(p, log(h))), '-');
+xlabel('Skrefastærð h');
+ylabel('Orkutap/Skekkja Error');
+%legend('Error(h)','Besta lína');
+legend('Error(h)', ['Besta lína (halli: ', num2str(p(1)), ')']);
+title('Orkutap sem fall af skrefastærð. Log skala graf. ');
+grid on;
+hold off;
+% Vista mynd
+exportgraphics(gcf,'myndir/part7_6.pdf');
 % Prentum hallann
 disp(['Hallatala línunnar er: ', num2str(p(1))]);
 
