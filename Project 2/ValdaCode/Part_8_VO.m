a2caa111cedbe86724534a1c540c69f26dde0186
vo23vo23 <valdimaro23@ru.is>
2023-12-09 15:34:50 +0000

    Dæmi 8 (grafið) lagfært örlítið

--- Project 2/ValdaCode/Part_8_VO.m
+++ Project 2/ValdaCode/Part_8_VO.m
@@ -7,7 +7,7 @@ Leq=L0+m*g/k;
 s0=[L0,L0,2*L0,3*L0,-L0,-2*L0,-2*L0,-3*L0]; %Upphafsgildin, 8 talsins
 s0_string={'L0','L0','2*L0','3*L0','-L0','-2*L0','-2*L0','-3*L0'};
 theta0=[pi*3/4,pi/3,pi/2,pi,pi,pi*2/3,pi/36,pi/2];  %Upphafsgildin, 8 talsins
-theta0_string={'\pi', '\pi/3', '\pi/2', '\pi', '\pi', '2\pi/3', '\pi/36', '\pi/2'};
+theta0_string={'3\pi/4', '\pi/3', '\pi/2', '\pi', '\pi', '2\pi/3', '\pi/36', '\pi/2'};
 T=40;
 n=10;
 format long
@@ -20,40 +20,41 @@ N_vector=zeros(1,length(Nu));
 N_matrix=NaN(length(s0),length(Nu));    %Array to store each N_vector
 %iter=0
 for j=1:length(s0)
-%    iter=iter+1
-for i=1:Nu
-N=(2^(i-1))*n*T;
-N_vector(i)=N;  % Concat the N value for the X-axis (plot)
-w=EulersolverRK(s0(j),theta0(j),T,N);
-
-x_T=w(:,1);
-vx_T=w(:,2);
-y_T=w(:,3);
-vy_T=w(:,4);
-
-L=sqrt(x_T.^2+y_T.^2); %calculates length of spring at all time steps
-
-% Calculate initial energy
-U_initial = m * g * y_T(1);
-K_initial = 0.5 * m * (vx_T(1)^2 + vy_T(1)^2);
-W_initial = 0.5 * k * (L0 - L(1))^2;
-E_initial = U_initial + K_initial + W_initial;
-
-%Calculate the energy values
-U=m*g*y_T;
-K=0.5*m*(vx_T.^2+vy_T.^2);
-W=0.5*k*(L0-L).^2;
-E=U+K+W;
-
-%Error=abs(E-E_initial);
-E_final=E(N);
-%E_f(i)=Error(N+1)% Add the error value to the E_f vector for y-axis
-E_f(i)=abs(E_final-E_initial)% Add the error value to the E_f vector for y-axis 
-end
+    %    iter=iter+1
+    for i=1:Nu
+    N=(2^(i-1))*n*T;
+    N_vector(i)=N;  % Concat the N value for the X-axis (plot)
+    w=EulersolverRK(s0(j),theta0(j),T,N); %Run the eulersolver with RK method for this initial condition
+    
+    x_T=w(:,1);
+    vx_T=w(:,2);
+    y_T=w(:,3);
+    vy_T=w(:,4);
+    
+    L=sqrt(x_T.^2+y_T.^2); %calculates length of spring at all time steps
+    
+    % Calculate initial energy
+    U_initial = m * g * y_T(1);
+    K_initial = 0.5 * m * (vx_T(1)^2 + vy_T(1)^2);
+    W_initial = 0.5 * k * (L0 - L(1))^2;
+    E_initial = U_initial + K_initial + W_initial;
+    
+    %Calculate the energy values
+    U=m*g*y_T;
+    K=0.5*m*(vx_T.^2+vy_T.^2);
+    W=0.5*k*(L0-L).^2;
+    E=U+K+W;
+    
+    %Error=abs(E-E_initial);
+    E_final=E(N);
+    %E_f(i)=Error(N+1)% Add the error value to the E_f vector for y-axis
+    E_f(i)=abs(E_final-E_initial)% Add the error value to the E_f vector for y-axis 
+    end
 Ef_matrix(:,j)=E_f;
 N_matrix(:,j)=N_vector;
 end
 
+figure('Position', [100, 100, 1024, 768]); % Adjust the last two values to set the width and height.
 for i = 1:length(s0)
 % Calculate the slope of the line in the log-log plot
 p = polyfit(log(N_matrix(:,i)), log(Ef_matrix(:,i)), 1); % p(1) contains the slope
