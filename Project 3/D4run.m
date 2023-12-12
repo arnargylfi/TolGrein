e2eb4263c57a7966ae2b430f7324353011ba252b
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-12 12:39:08 +0000

    Bætti við hreyfimynd í dæmi 4

--- Project 3/D4run.m
+++ Project 3/D4run.m
@@ -8,4 +8,19 @@ mesh(x, t, W')
 xlabel("x [m]")
 ylabel("t [s]")
 zlabel("V [V]")
-exportgraphics(gcf,"Myndir/D4.pdf")
+% exportgraphics(gcf,"Myndir/D4.pdf")
+
+% HREYFIMYND 
+
+
+for j = 1:n+1
+    clf;
+    hold on
+    w = W(:,j);
+    plot(x,w)
+    ylim([0,max(max(W))])
+    xlabel('x[m]')
+    ylabel('V [V]')
+    title("t = " +t(j)+"s")
+    pause(0.1);
+end % for
