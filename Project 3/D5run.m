a8947c1527099d29678c300839846792559fdaff
Arnar Gylfi <arnargylfi01@gmail.com>
2023-12-12 13:12:49 +0000

    commit af því skrýtið seinast

---
+++ Project 3/D5run.m
@@ -0,0 +1,26 @@
+format long
+T= 10^-2;
+m = 5000;
+n = 5000;
+[W, x, t] = D4(T,m,n);
+ka = max(W(:, end));
+mesh(x, t, W')
+xlabel("x [m]")
+ylabel("t [s]")
+zlabel("V [V]")
+exportgraphics(gcf,"Myndir/D5.pdf")
+
+% HREYFIMYND 
+% 
+% 
+% for j = 1:n+1
+%     clf;
+%     hold on
+%     w = W(:,j);
+%     plot(x,w)
+%     ylim([0,max(max(W))])
+%     xlabel('x[m]')
+%     ylabel('V [V]')
+%     title("t = " +t(j)+"s")
+%     pause(0.1);
+% end % for
